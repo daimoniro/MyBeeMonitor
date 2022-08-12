5722a1620e63da59d2e86f2c0dbcaccdf2440f99
Lorenzo Joale <lorenzo.joale@solidpower.com>
2022-08-12 14:07:58 +0200

    Add temperature module

--- source/temperature.c
+++ source/temperature.c
@@ -13,8 +13,14 @@
 #include <pthread.h>
 #include <unistd.h>
 
+#include "debug.h"
 
 /* Private defines ------------------------------------------------------------*/
+
+#define FREQUENZA_LETTURA_TEMPERATURE	5
+
+#define ID_SENDORE_INTERNO	0
+
 
 /* Private macros -------------------------------------------------------------*/
 
@@ -26,10 +32,17 @@
 
 /* Private variables ----------------------------------------------------------*/
 
-/* Public variables -----------------------------------------------------------*/
+static char debug_str_temp[256];
+static const char delim[] = "=";
 
+/* Public variables -----------------------------------------------------------*/
+int valoriTemperatura[3];
+char pathDeviceTemperatureSensor[64];
+
+float tempDS18D20 = 0;
 /* Private function prototypes ------------------------------------------------*/
 void *temperature_management();
+int letturaTemperatura(int idSensore);
 
 /* Private function bodies ----------------------------------------------------*/
 
@@ -52,9 +65,153 @@ void StartTemperatureManagement()
 //-----------------------------------------------------------------------------
 void *temperature_management()
 {
+    time_t now;
+	int temperatureLette = 0;
+
+
+	int returnFunz = 0;
+
+
+	//system("sudo modprobe w1-gpio");
+	//system("sudo modprobe w1-therm");
+
+
+	sprintf(pathDeviceTemperatureSensor,"/sys/bus/w1/devices/28-00000620f601/w1_slave");
+	//sprintf(pathDeviceTemperatureSensor,"/sys/bus/w1/devices/%s/w1_slave",cfg.idTemperatureSensor);
+
+	valoriTemperatura[ID_SENDORE_INTERNO] = 0xFFFF;
+	valoriTemperatura[1] = 0xFFFF;
+	valoriTemperatura[2] = 0xFFFF;
+
+	letturaTemperatura(ID_SENDORE_INTERNO);
+
     while(true)
     {
-        sleep(1);
-        printf("Do stuff ...\n");
+
+        usleep(100000);
+		time(&now);
+		printf("now: %ld\n",now);
+
+
+		if((now % FREQUENZA_LETTURA_TEMPERATURE) == 0)
+		{
+
+			if(temperatureLette == 0)
+			{
+				returnFunz = letturaTemperatura(ID_SENDORE_INTERNO);
+
+				if(returnFunz >0 )
+				{
+					sprintf(debug_str_temp,"valoreTemperatura %d",valoriTemperatura[0]);
+					TRACE4(1,"TEMPERATURE",BIANCO,NERO_BG,debug_str_temp,0);
+
+					tempDS18D20 = (float)((short)valoriTemperatura[0])/((float)1000);
+				}
+				else
+				{
+					sprintf(debug_str_temp,"Errore lettura temperatura. path: %s",pathDeviceTemperatureSensor);
+					TRACE4(1,"TEMPERATURE",NERO,ROSSO_BG,debug_str_temp,0);
+				}
+				temperatureLette = 1;
+			}
+		}
+		else
+		{
+			temperatureLette = 0;
+		}
     }
 }
+
+
+
+//---------------------------------------------------------------------------------------
+// letturaTemperatura
+//---------------------------------------------------------------------------------------
+int letturaTemperatura(int idSensore)
+{
+	FILE * SensoreFile;
+	char Line[256];
+	char *svptr;						// puntatore aggiornato dalla funzione strtok_r
+	char *s;
+
+	int lineNumber = 0;
+	int yesLetto = 0;
+
+
+	switch(idSensore)
+	{
+		case ID_SENDORE_INTERNO:
+			SensoreFile = fopen(pathDeviceTemperatureSensor,"r");
+			break;
+		/*case ID_SENDORE_ESTERNO:
+			SensoreFile = fopen(FILE_SENSORE_1W_ESTERNO,"r");
+			break;
+		case ID_SENDORE_FERMENTATORE:
+			SensoreFile = fopen(FILE_SENSORE_1W_FERMENTATORE,"r");
+			break;*/
+
+        default:
+           valoriTemperatura[idSensore] = 0xFFFF;
+		    return -1;
+
+	}
+
+
+	if(SensoreFile==NULL)
+	{
+		//printf("Impossibile leggere file con ID %d\n", idSensore);
+
+		valoriTemperatura[idSensore] = 0xFFFF;
+		return -1;
+	}
+
+	while(fgets(Line,256,SensoreFile)!=NULL)
+	{
+		printf("Leggo %s da file %s\n",Line,pathDeviceTemperatureSensor);
+
+		//Leggo 15 00 4b 46 7f ff 0b 10 a4 : crc=a4 YES
+		//Leggo 15 00 4b 46 7f ff 0b 10 a4 t=1312
+
+		if((Line[0]!='#')&&(Line[0]!='\0')&&(Line[0]!='\n'))
+		{
+
+
+			if(lineNumber == 0)
+			{
+				if(strstr(Line,"YES") != NULL)
+				{
+					yesLetto = 1;
+				}
+			}
+
+			if(lineNumber == 1)
+			{
+				if(yesLetto == 1)
+				{
+					//printf("YES letto\n");
+					s = strtok_r(Line,delim,&svptr);
+					s=strtok_r(NULL,delim,&svptr);
+					//printf("Dopo = --> %d\n",atol(s));
+
+					valoriTemperatura[idSensore] = atol(s);
+
+				}
+				else
+				{
+					valoriTemperatura[idSensore] = 0xFFFF;
+				}
+
+
+			}
+
+			lineNumber++;
+		}
+
+	}
+
+	fclose(SensoreFile);
+
+	return 1;
+
+}
+
