dee9814fac017d6110c49ff90e8165dc1aa7ae79
daimoniro <lorenzo.joale@gmail.com>
2023-03-21 10:47:21 +0100

    Add timestamp field to mqtt payload

--- source/json.c
+++ source/json.c
@@ -14,7 +14,7 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <jansson.h>
-
+#include <time.h>
 #include "apiario.h"
 
 /* Private defines ------------------------------------------------------------*/
@@ -36,32 +36,57 @@
 /* Private function bodies ----------------------------------------------------*/
 extern apiario_t apiario;
 
+//----------------------------------------------------------------------
+//  gettime_4log_journal
+//----------------------------------------------------------------------
+void gettime_4log_journal(unsigned char *pubTime)
+{
+    time_t stCurTime;
+    struct tm *stTimeInfo;
+    struct timeval stTimeval;
+    unsigned char aubTime[48];
+
+    time(&stCurTime);
+    gettimeofday(&stTimeval, NULL);
+
+    stTimeInfo = gmtime(&stCurTime);
+    strftime((char *)aubTime, 48, "%Y-%m-%d %H:%M:%S", stTimeInfo);
+
+    sprintf((char *)pubTime, "%s.%03ld", aubTime, stTimeval.tv_usec / 1000);
+
+}
+
 char *build_get_apiario_json()
 {
+	unsigned char timestamp[48];
+	gettime_4log_journal(timestamp);
+
 	json_t *obj = json_pack("{"
+							"s:{s:s},"      //Timestamp
 							"s:{s:f,s:f,s:f},"      //common
                             "s:{s:f,s:f,s:f,s:f},"  //arnia_1
                             "s:{s:f,s:f,s:f,s:f},"  //arnia_2
                             "s:{s:f,s:f,s:f,s:f}"   //arnia_3
 							"}",
-
+							"Timestamp",
+								"ts",timestamp,
 							"common",
 							    "rain", apiario.rain,
 							    "temperature_external", apiario.temperature_external,
                                 "lux",apiario.lux,
                             "arnia_1",
 							    "temperature_internal", apiario.arnie[0].temperature_internal,
-								"internal_2", apiario.arnie[0].temperature_internal_2,
+								"temperature_internal_2", apiario.arnie[0].temperature_internal_2,
 							    "humidity_internal", apiario.arnie[0].humidity_internal,
                                 "weight", apiario.arnie[0].weight,
                             "arnia_2",
 							     "temperature_internal", apiario.arnie[1].temperature_internal,
-								 "internal_2", apiario.arnie[1].temperature_internal_2,
+								 "temperature_internal_2", apiario.arnie[1].temperature_internal_2,
 							    "humidity_internal", apiario.arnie[1].humidity_internal,
                                 "weight", apiario.arnie[1].weight,
                             "arnia_3",
 							     "temperature_internal", apiario.arnie[2].temperature_internal,
-								 "internal_2", apiario.arnie[2].temperature_internal_2,
+								 "temperature_internal_2", apiario.arnie[2].temperature_internal_2,
 							    "humidity_internal", apiario.arnie[2].humidity_internal,
                                 "weight", apiario.arnie[2].weight
 	);
@@ -69,7 +94,6 @@ char *build_get_apiario_json()
 	
 	char *json = json_dumps(obj, JSON_PRESERVE_ORDER | JSON_REAL_PRECISION(4));
 
-	//printf("json: %s\n",json);
 	json_decref(obj);
 	return json;
 }
