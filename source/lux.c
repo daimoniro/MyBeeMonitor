801589ddd0c09f0bacd61cc5891c080f677086f8
Lorenzo Joale <lorenzo.joale@solidpower.com>
2023-03-24 15:00:19 +0100

    First version reading Lux sensor

--- source/lux.c
+++ source/lux.c
@@ -12,10 +12,15 @@
 #include <stdbool.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <pigpio.h>
 
 #include "apiario.h"
 /* Private defines ------------------------------------------------------------*/
+#define I2C_LUX_ADDR    0x23
+#define I2C_LUX_REG_READ        0
+#define I2C_LUX_REG_WRITE       0
 
+#define I2C_LUX_CONFIGURATION_BYTE  0x10
 /* Private macros -------------------------------------------------------------*/
 
 /* Private data types ---------------------------------------------------------*/
@@ -25,7 +30,7 @@
 /* Private structures ---------------------------------------------------------*/
 
 /* Private variables ----------------------------------------------------------*/
-
+int lux_i2c_handle = 0;
 /* Public variables -----------------------------------------------------------*/
 extern apiario_t apiario;
 
@@ -53,11 +58,31 @@ void StartLuxManagement()
 //-----------------------------------------------------------------------------
 void *lux_management()
 {
+    
+    lux_i2c_handle = i2cOpen(1,I2C_LUX_ADDR,0);
+
+    if(lux_i2c_handle < 0)
+    {
+         printf("Sensore luce non disponibile \n");   
+         apiario.lux = -1;
+         return;
+    }
+    else
+    {
+        printf("Lux I2C handle: %d\n",lux_i2c_handle);
+        int ret = i2cWriteByte(lux_i2c_handle,I2C_LUX_CONFIGURATION_BYTE);
+
+        printf("ret i2cWriteByte: %d\n",ret);
+    }
+
     while(true)
     {
         sleep(1);
 
-        apiario.lux = 160.7;
+        int word=i2cReadWordData(lux_i2c_handle,I2C_LUX_REG_READ);
+        int lux=((word & 0xff00)>>8) | ((word & 0x00ff)<<8);
+        printf("word: %d --> lux : %d\n",word,lux);
+        apiario.lux = (float)lux;
         //printf("Do stuff ...\n");
     }
 }
