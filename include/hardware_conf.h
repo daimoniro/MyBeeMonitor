eec57a088093cf24397f357ee138dff58aadfee9
Lorenzo Joale <lorenzo.joale@solidpower.com>
2023-03-28 14:35:33 +0200

    Add header file for hardware configuration

---
+++ include/hardware_conf.h
@@ -0,0 +1,15 @@
+/**
+ ******************************************************************************
+ * @file    	hardware_conf.h
+ * @author  	Lorenzo Joale'
+ * @date    	12 Aug 2022
+ *******************************************************************************
+ */
+
+#ifndef _HARDWARE_CONF_H
+#define _HARDWARE_CONF_H
+
+//Sensori umidita'
+#define DHT_PIN_1		22
+
+#endif /* _HARDWARE_CONF_H */
